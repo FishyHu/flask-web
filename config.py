c8eb0f279ae40804c0b704bc3a8a49e7bb435aa7
ikanasin <sherly28@gmail.com>
2020-11-06 15:08:38 +0700

    try to add this to github

--- config.py
+++ config.py
@@ -5,6 +5,12 @@ class base(object):
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SECRET_KEY = os.environ['SECRET_KEY']
 
+class test(base):
+	TESTING = True
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+	WTF_CSRF_ENABLED = False
+
 class demo(base):
 	DEBUG = True
 
